d6e369e6a67df6f5c41f12551b09cc6a426767fb
Chimwemwe Phikiso <33883304+cphikiso@users.noreply.github.com>
2023-05-02 19:44:17 +0100

    ENV file

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,19 @@
+/* eslint-disable linebreak-style */
 const functions = require("firebase-functions");
 const {Configuration, OpenAIApi} = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 exports.callDavinci = functions.https.onRequest(async (req, res) => {
   try {
+    const prompt = req.body.prompt;
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Hello world",
+      prompt: prompt,
     });
     console.log("text", completion.data.choices[0].text);
     res.json(completion.data);
@@ -24,4 +26,25 @@ exports.callDavinci = functions.https.onRequest(async (req, res) => {
     }
   }
 });
+exports.callDavinci = functions.https.onRequest(async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+    });
+    console.log("text", completion.data.choices[0].text);
+    res.json(completion.data);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+});
+
+
+
 
